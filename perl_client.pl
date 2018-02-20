7efeb1c665cce9ba09bc724d7645bddfcd221caa
Rafid Saad <Bazarovay@users.noreply.github.com>
2018-02-20 14:41:18 -0500

    Creating perl client

---
+++ perl_client.pl
@@ -0,0 +1,40 @@
+use IO::Socket::INET;
+use Crypt::CBC;
+use MIME::Base64;
+# use Data::Dumper qw(Dumper);
+
+# print Dumper \@ARGV;
+$| = 1;
+
+# create a connecting socket
+sub make_remote_connection {
+  my $socket = new IO::Socket::INET (
+      PeerHost => '0.0.0.0',
+      PeerPort => '8321',
+      Proto => 'tcp',
+  );
+  die "cannot connect to the server $!\n" unless $socket;
+  print "connected to the server\n";
+
+  return $socket
+}
+my $our_secret = "MYSECRET";
+# data to send to a server
+
+while (1) {
+    my $socket = make_remote_connection();
+    my $req = <STDIN>;
+    my $req = $req.$our_secret;
+    my $size = $socket->send($req);
+    shutdown($socket, 1);
+
+    # receive a response of up to 1024 characters from server
+    my $response = "";
+    $socket->recv($response, 1024);
+    print "Remote machine: $response\n";
+
+    # print $socket $msg;
+    # print scalar <$socket>;
+}
+$socket->close()
+    or die "Can't close socket ($!)\n";
